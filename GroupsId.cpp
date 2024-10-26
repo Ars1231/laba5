1ac16ce02957987555ba8c9d137c4f6dca88c275
Ars1231 <84440425+Ars1231@users.noreply.github.com>
2024-10-26 15:25:59 +0300

    Create GroupsId.cpp

---
+++ GroupsId.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+using namespace std;
+
+struct Student {
+    std::string Name;
+    std::string LastName;
+    std::string GroupId;
+    std::vector<unsigned> Ratings; 
+    std::vector<std::string> Subjects;
+};
+
+
+std::vector<std::string> GroupsId(const std::vector<Student>& students) {
+    std::set<std::string> uniqGroups; 
+
+    for (const auto& student : students) {
+        uniqGroups.insert(student.GroupId); 
+    }
+    
+	vector<std::string> groupsVector(uniqGroups.begin(), uniqGroups.end());
+    return groupsVector;
+}
+
+int main() {
+    vector<Student> students = {
+        {"Anna", "Ivanova", "Group1", {5, 4}, {"Math", "Physics"}},
+        {"Ivan", "Vitaly", "Group2", {2, 4}, {"Biology", "Chemistry"}},
+        {"Ars", "Potapov", "Group1", {5, 5}, {"Math", "Literature"}},
+        {"Maria", "Maurina", "Group3", {4, 4}, {"Math", "History"}}
+    };
+
+    vector<string> uniqGroups = GroupsId(students);
+    
+    cout << "Разные группы:" << endl;
+    for (const auto& group : uniqGroups) {
+        cout << group << endl; 
+    }
+
+    return 0;
+}
